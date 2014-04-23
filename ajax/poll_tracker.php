f7e245957b6f1a556378eeb5862a1b7ec41f18d6
Chris Dickow <chrisdickow@gmail.com>
2014-04-23 16:55:44 -0400

    Tracking working

---
+++ ajax/poll_tracker.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../includes/config.php';
+include '../includes/opendb.php';
+
+
+if(isset($_GET['session_key'])){
+	
+	$session_key = $_GET['session_key'];
+
+	$sql = "SELECT * FROM positions WHERE ninja_session_key = '$session_key' ORDER BY ninja_update_time DESC LIMIT 1";
+	$result = mysql_query($sql) or die(mysql_error());
+
+	while($row = mysql_fetch_assoc($result)){
+		$lat = $row['ninja_lat'];
+		$lng = $row['ninja_lng'];
+		$timestamp = $row['ninja_update_time'];
+		$key = $row['ninja_session_key'];
+	}
+	// mail($recipient, $user . ' wants you to track them!', $msg, $headers);
+
+	// Everything is squared away. Link sent, first set of coords in db.
+	echo '{"lat": "'. $lat .'", "lng": "'. $lng .'", "session_key": "'. $session_key .'"}';
+}
+
+
+?>
