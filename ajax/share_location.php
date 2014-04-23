581b4d49505c8e2be564ff2f39ad07f4e1d55c42
Chris Dickow <chrisdickow@gmail.com>
2014-04-23 16:22:18 -0400

    Half-baked and sort of working. A little refinement and I'll be there

--- ajax/share_location.php
+++ ajax/share_location.php
@@ -1,6 +1,7 @@
 <?php
 
-
+include '../includes/config.php';
+include '../includes/opendb.php';
 
 
 if(isset($_POST['user'])){
@@ -12,21 +13,21 @@ if(isset($_POST['user'])){
 	// Add new record to the db
 	$session_key = md5($user.$lat.$lng);
 
-	$sql = "INSERT INTO geo_ninja (user, lat, lng, session_key) VALUES ($user, $lat, $lng, $session_key)";
+	$sql = "INSERT INTO positions (ninja_username, ninja_lat, ninja_lng, ninja_session_key) VALUES ('$user', '$lat', '$lng', '$session_key')";
 	$result = mysql_query($sql) or die(mysql_error());
 
-	$link = "track.php?k=" . $session_key;
+	$link = "http://www.project107.net/geo-ninja/?lat=". $lat ."&lng=". $lng ."&user=". $user ."&id=". $session_key;
 	$msg = "geo-ninja tracking \n".
 			$user . " wants you to track them with the geo-ninja app. To see where they're at, click the tracking link below: \n".
 			$link;
-	$headers = 'From: tracker@project-107.net' . "\r\n" .
-    'Reply-To: chris@project-107.net' . "\r\n" .
+	$headers = 'From: tracker@project107.net' . "\r\n" .
+    'Reply-To: chris@project107.net' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-	mail($recipient, $user . ' wants you to track them!', $msg, $headers);
+	mail($recipient, $user . ' wants you to track them!', $msg, $headers) or die("Failure on mail");
 
 	// Everything is squared away. Link sent, first set of coords in db.
-	echo "[session_key: '$session_key']";
+	echo '{"session_key": "48deeda8a3f2984670bd316057a8fd5f"}';
 }
 
 
