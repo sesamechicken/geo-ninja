b2c23b587fd67dd4601d120caddf8035235d57d5
Chris Dickow <chrisdickow@gmail.com>
2014-04-24 10:18:28 -0400

    Sounds logic. Tweak and cleanup from here

--- ajax/share_location.php
+++ ajax/share_location.php
@@ -1,6 +1,7 @@
 <?php
 
-
+include '../includes/config.php';
+include '../includes/opendb.php';
 
 
 if(isset($_POST['user'])){
@@ -9,24 +10,30 @@ if(isset($_POST['user'])){
 	$lng = $_POST['lng'];
 	$recipient = $_POST['recip'];
 
+	// Clean up vars 
+	$encoded_user = urlencode($user);
+
 	// Add new record to the db
 	$session_key = md5($user.$lat.$lng);
 
-	$sql = "INSERT INTO geo_ninja (user, lat, lng, session_key) VALUES ($user, $lat, $lng, $session_key)";
+	$sql = "INSERT INTO positions (ninja_username, ninja_lat, ninja_lng, ninja_session_key) VALUES ('$user', '$lat', '$lng', '$session_key')";
 	$result = mysql_query($sql) or die(mysql_error());
 
-	$link = "track.php?k=" . $session_key;
-	$msg = "geo-ninja tracking \n".
-			$user . " wants you to track them with the geo-ninja app. To see where they're at, click the tracking link below: \n".
+	$link = "http://www.project107.net/geo-ninja/?lat=". $lat ."&lng=". $lng ."&user=". $encoded_user ."&id=". $session_key;
+	$msg = "<h2>geo-ninja tracking</h2> \n".
+			"<p>". $user . " wants you to track them with the geo-ninja app. To see where they're at, click the tracking link below: </p> \n".
 			$link;
-	$headers = 'From: tracker@project-107.net' . "\r\n" .
-    'Reply-To: chris@project-107.net' . "\r\n" .
+	
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: tracker@project107.net' . "\r\n" .
+    'Reply-To: chris@project107.net' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-	mail($recipient, $user . ' wants you to track them!', $msg, $headers);
+	mail( $recipient, $user . ' wants you to track them!', $msg, $headers ) or die("Failure on mail");
 
 	// Everything is squared away. Link sent, first set of coords in db.
-	echo "[session_key: '$session_key']";
+	echo '{"session_key": "'. $session_key .'"}';
 }
 
 
---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+// development
+/* 
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'geo_ninja';
+*/
+
+// Production 
+$dbhost = 'db525235557.db.1and1.com';
+$dbuser = 'dbo525235557';
+$dbpass = '!Frank8179?';
+$dbname = 'db525235557';
+
+?>
